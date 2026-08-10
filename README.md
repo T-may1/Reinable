4dd5b5ecaf691c90a86e9cba75b7dfadaffffd15
T-may1 <timei0208@gmail.com>
2026-08-10 17:41:41 +0200

    Backend to Frontend communication re-implemented, data exchange for updateProfile successful

--- README.md
+++ README.md
@@ -14,40 +14,6 @@ To serve frontend from backend execute :
 app runs on port 3000
 
 
-async created() {
-        const profileData = await this.fetchProfileData()
-        this.name = profileData.name
-        this.role = profileData.role
-        this.schooling = profileData.schooling
-        this.height = profileData.height
-        this.weight = profileData.weight
-    },
 
 
-async handleUpdateProfile() {
-            const payload = {
-                name: this.name,
-                role: this.role,
-                shcooling: this.schooling,
-                height: this.height,
-                weight: this.weight
-            }
-            const resJson = await this.updateProfileData(payload)
-            console.log(resJson)
-            this.isEditMode = false
-        },
-        async fetchProfileData() {
-            const res = await fetch('get-profile')
-            return await res.json()
-        },
-        async updateProfileData(payload) {
-            const res = await fetch('update-profile', {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: JSON.stringify(payload)
-            })
-            return await res.json()
-        }
+
--- src/Profile.vue
+++ src/Profile.vue
@@ -70,13 +70,45 @@ export default {
         }
        
     },
-    
+    async created() {
+        const profileData = await this.fetchProfileData()
+        this.name = profileData.name
+        this.role = profileData.role
+        this.schooling = profileData.schooling
+        this.height = profileData.height
+        this.weight = profileData.weight
+    },
     methods: {
         handleEditProfile() {
             this.isEditMode = true
             },
-        handleUpdateProfile() {
+        async handleUpdateProfile() {
+            const payload = {
+                name: this.name,
+                role: this.role,
+                shcooling: this.schooling,
+                height: this.height,
+                weight: this.weight
+            }
+            const resJson = await this.updateProfileData(payload)
+            console.log(resJson)
+            
             this.isEditMode = false
+        },
+        async fetchProfileData() {
+            const res = await fetch('get-profile')
+            return await res.json()
+        },
+        async updateProfileData(payload) {
+            const res = await fetch('update-profile', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+            return await res.json()
         }
         
     },
